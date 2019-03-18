f755240b0eb481220cb97d20c4c53b16398f266d
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-03-18 10:32:58 -0400

    Minor change to README (not sure why I did this), and made some description changes to the CLI script. This will all probably change soon with new architecture

--- precis/cli.py
+++ precis/cli.py
@@ -1,32 +1,28 @@
-from types import SimpleNamespace
 import argparse
-import sys
 
 
 def main():
-    args_parsed = buildArgparse()
-
-
-def buildArgparse() -> SimpleNamespace:
     parser = argparse.ArgumentParser(
         prog='precis',
-        description='The non-redundant resume engine'
+        description='The non-redundant resume engine.'
     )
 
     # Required arguments
     parser.add_argument('data_file', action='store', nargs=1,
-                        help='Data file to be used for resume')  # data file
+                        help='Data file to be used for resume. Must be in \
+                        Precis-compatible format.')  # data file
     parser.add_argument('template_file', action='store', nargs=1,
-                        help='Template file to be compiled')  # template file
+                        help='Template file to be compiled.')  # template file
 
     # Optional argument - overriding data
     parser.add_argument('--override', action='append', nargs='+',
-                        help='File to be used to override main data file')
+                        help='File(s) to be used to override main data file. \
+                        In ascending order of specificity; i.e. data_file < \
+                        override 1 < override 2 < ... < override n.')
 
-    print(parser.parse_args())
+    args = parser.parse_args()
 
-    return parser.parse_args()
-
+    print(args.data_file)
 
 if __name__ == '__main__':
     main()
