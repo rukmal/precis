d6a71cb41964abe234b8e7d0f9245187eb630d7a
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-17 12:45:35 -0400

    Removed old file (about to delete module; reworking tempalting engine structure), and added blank test shell for the new templating engine

---
+++ tests/template_test.py
@@ -0,0 +1,10 @@
+from test_cfg import TestConfig
+from context import precis
+
+import unittest
+
+
+class TestTemplate(unittest.TestCase):
+    
+    def test_template(self):
+        self.assertTrue(True)
--- tests/templater_test.py
+++
@@ -1,8 +0,0 @@
-from test_cfg import TestConfig
-from context import precis
-
-import unittest
-
-
-class TestTemplater(unittest.TestCase):
-    pass
