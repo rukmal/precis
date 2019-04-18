c4cab9cc7aa84a61d3169bc524734763218a6e6c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-18 14:00:52 -0400

    Added docstrings to the templatedriver module; ready to start templating

--- precis/templating/driver.py
+++ precis/templating/driver.py
@@ -11,9 +11,41 @@ import logging
 
 
 class TemplateDriver():
+    """This module encapsulated functionality to ingest user data, user template
+    preferences, a template, and to render the template based on that data.
 
+    TemplateDriver ingests user template preferences, and checks them for
+    validity, both against the content of the supplied user data ontology, and
+    the content of the template configuration.
+    """
+    
     def __init__(self, template: Template, user_ont: Ontology,
                  user_graph: Graph, user_prefs: TextIOWrapper):
+        """TemplateDriver initialization method. Validates user preferences
+        against the supplied ontology, and against the template configuration.
+
+        This initialization method also builds user data to be passed to the
+        template, but does not start the build process. This data is constructed
+        using restrictions from the user preferences, and only required classes
+        from the template configuraion. Intelligent logging and error messages
+        pinpoint errors in user preferences for easy debugging.
+        
+        Arguments:
+            template {Template} -- Template to be rendered.
+            user_ont {Ontology} -- User data ontology.
+            user_graph {Graph} -- RDFLib graph representation of the ontology.
+            user_prefs {TextIOWrapper} -- User template preferences file.
+    
+        Raises:
+            AttributeError -- Raised when a attribute required by the template
+                            configuration is missing from the user preferences.
+            KeyError -- Raised when an invalid class type or class ID is used in
+                        preferences that does not exist in the user ontology.
+            ParserError -- Raised when the user preferences has malformed
+                           YAML syntax.
+            ValueError -- Raised when an invalid ordering scheme is specified.
+        """
+        
         # Parsing user preferences, saving to class variable
         try:
             self.user_prefs = yaml_load(stream=user_prefs, Loader=SafeLoader)
@@ -65,12 +97,32 @@ class TemplateDriver():
                 class_invds = [i for i in class_invds if i['$id']
                     in item_overrides[ont_class]]
 
+            logging.debug('Extracted {0} individuals of class {1} for template'\
+                .format(len(class_invds), ont_class))
+
             # Appending to user data dictionary
             self.user_data[ont_class] = class_invds
         
-        print(self.user_data)
+        # Appending required fields from user preferences to user data
+        for field in template.getRequiredInput():
+            self.user_data[field] = self.user_prefs[field]
+
+        logging.info('Successfully built user data object for template\
+            rendering with {0} fields'.format(len(self.user_data.keys())))
 
     def __getItemOverrides(self) -> dict:
+        """Function to get specific item overrides, in a dictionary of the form
+        {OntologyClass: [item_id_1, item_id_2, ...]}. This function also
+        validates that listed IDs are valid, and that they exist in the ontology
+        and are of the correct type.
+        
+        Raises:
+            KeyError -- Raised when an incorrect class type or class ID is used.
+        
+        Returns:
+            dict -- Dictionary of item overrides.
+        """
+
         # Only proceed if item overrides are specified
         if 'item_overrides' not in self.user_prefs_attrs:
             return {}
@@ -97,6 +149,18 @@ class TemplateDriver():
         return self.user_prefs['item_overrides']
 
     def __getOrderOverrides(self) -> dict:
+        """Function to get order overrides for each type. Evaluates that the
+        ordering type specified is valid, and returns a dictionary of the form
+        {OntologyClass: OrderType} for each override specified in the
+        user preferences.
+        
+        Raises:
+            ValueError -- Raised if an invalid order override is specified.
+        
+        Returns:
+            dict -- Dictionary of order overrides.
+        """
+
         # Only proceed if order overrides are specified
         if 'order_overrides' not in self.user_prefs_attrs:
             return {}
