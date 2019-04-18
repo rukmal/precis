d876c92a0890a3a80ca8f1765581bc736712b25f
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-18 08:57:35 -0400

    Added basic getters for template information (will be useful when I get to writing the CLI)

--- precis/templating/template.py
+++ precis/templating/template.py
@@ -27,6 +27,52 @@ class Template():
             logging.debug('Loaded template configuration file {0}'.format(
                 f.name))
 
+    def getTemplateConfiguration(self) -> dict:
+        """Function to get the complete template configuration.
+        
+        Returns:
+            dict -- Template configuration.
+        """
+
+        return self.template_config
+
+    def getName(self) -> str:
+        """Function to get the full name of the template.
+        
+        Returns:
+            str -- Full name of the template.
+        """
+
+        return self.template_config['full_name']
+    
+    def getDescription(self) -> str:
+        """Function to get the description of the template.
+        
+        Returns:
+            str -- Template description.
+        """
+
+        return self.template_config['description']
+    
+    def getAuthor(self) -> str:
+        """Function to get the author of the template.
+        
+        Returns:
+            str -- Template author.
+        """
+
+        return self.template_config['author']
+
+    def getIncludedClasses(self) -> list:
+        """Function to get the included classes (from the Precis ontology) used
+        in the template.
+        
+        Returns:
+            list -- Classes (from Precis Ontology) included in the template.
+        """
+
+        return self.template_config['required_input']
+
     def render(self, render_data: dict):
         # render the template here
         pass
